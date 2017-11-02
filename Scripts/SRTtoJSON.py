2080002b8adc4c783a8fb54a59def6a6bf7a1b46
Eduardo Merino <A01630088@itesm.mx>
2017-11-01 21:09:21 -0600

    minor fix scipt

--- Scripts/SRTtoJSON.py
+++ Scripts/SRTtoJSON.py
@@ -1,12 +1,11 @@
 import re
 import json
 
-num = ""
+num = 0
 StartTime = ""
 EndTime = ""
 text = ""
 a = []
-go = True
 
 def createObj(number, Start_Time, End_Time, word):
     global a
@@ -21,40 +20,30 @@ def lineRead (line):
     global StartTime
     global EndTime
     global text
-    global go
     patternNum = re.compile("^[0-9]+$")
     patternTime = re.compile("^[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}$")
     patternText = re.compile("^[a-zA-Z, ,\D]+$")
-    patternBlank = re.compile("^[[:blank:]]+$")
 
     if(patternNum.match(line) != None):
-        if go:
-            num = line
-            go = False
-        else:
-            createObj(num, StartTime, EndTime, text)
-            text = ""
-            go = True
+        num = int(line)
     elif(patternTime.match(line) != None):
         lineArr = line.split()
         StartTime = lineArr[0]
         EndTime = lineArr[2]
     elif(patternText.match(line) != None):
-        ##print("copy text: " + line)
-        text = text + line + ""
-        ##createObj(num, StartTime, EndTime, text)
-        ##text = ""
-    ##elif(len(line) == 0):
-    ##    print("end")
-    ##    createObj(num, StartTime, EndTime, text)
-    ##    text = ""
-    ##else:
-    ##    print("else")
+        text = text + line
+    #if(num !=  0 and StartTime != "" and EndTime != "" and text != "" and text != "\n"):
+    if(line == "\n"):
+        #print("else")
+        createObj(num, StartTime, EndTime, text)
+        text = ""
+        num = 0
+        StartTime = ""
+        EndTime = ""
     return
 
 with open("captionTest.txt", 'r') as f:
     for line in f:
-        ##print(line)
         lineRead(line)
 with open("result.json",'w') as f:
     json.dump(a, f, indent=2)
